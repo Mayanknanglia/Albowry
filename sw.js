48977fa49dc0d3995d5f36d9a42df732e0904c2e
Mayanknanglia <85097496+Mayanknanglia@users.noreply.github.com>
2026-07-28 01:22:33 +0530

    Upgrade service worker to version 16

--- sw.js
+++ sw.js
@@ -1,40 +1,104 @@
-var CACHE_NAME = 'albowry-v15';
-var urls = ['./', './index.html', './style.css', './mobile.css', './app.js', './manual.js', './history.js', './manifest.json', './logo.png'];
+// AL BOWRY CARPENTRY LLC - Service Worker v16
+var CACHE_NAME = 'albowry-v16';
+var ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './mobile.css',
+  './app.js',
+  './manual.js',
+  './history.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
+// Install
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urls).catch(function() {});
+      return cache.addAll(ASSETS);
+    }).catch(function(err) {
+      console.log('[SW] Cache install error:', err);
     })
   );
-  self.skipWaiting();
 });
 
+// Activate - clear old caches
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(function(names) {
-      var promises = [];
-      for (var i = 0; i < names.length; i++) {
-        if (names[i] !== CACHE_NAME) {
-          promises.push(caches.delete(names[i]));
-        }
-      }
-      return Promise.all(promises);
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
     })
   );
-  self.clients.claim();
 });
 
+// Fetch - Network first for Firebase/CDN, Cache first for assets
 self.addEventListener('fetch', function(e) {
   var url = e.request.url;
-  if (url.indexOf('firebase') > -1 || url.indexOf('firestore') > -1 || url.indexOf('googleapis') > -1 || url.indexOf('gstatic') > -1 || url.indexOf('cdnjs') > -1) {
+
+  // Skip Firebase, CDN, Google APIs - always network
+  if (
+    url.indexOf('firestore.googleapis.com') !== -1 ||
+    url.indexOf('firebase') !== -1 ||
+    url.indexOf('gstatic.com') !== -1 ||
+    url.indexOf('googleapis.com') !== -1 ||
+    url.indexOf('cdnjs.cloudflare.com') !== -1 ||
+    url.indexOf('fonts.googleapis.com') !== -1 ||
+    url.indexOf('fonts.gstatic.com') !== -1
+  ) {
     return;
   }
+
+  // For same-origin assets: Cache first, then network
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request).catch(function() {
+    caches.match(e.request).then(function(cached) {
+      if (cached) {
+        // Update cache in background
+        fetch(e.request).then(function(response) {
+          if (response && response.status === 200) {
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(e.request, response);
+            });
+          }
+        }).catch(function() {});
+        return cached;
+      }
+
+      // Not in cache - fetch from network
+      return fetch(e.request).then(function(response) {
+        if (response && response.status === 200 && e.request.method === 'GET') {
+          var responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(e.request, responseClone);
+          });
+        }
+        return response;
+      }).catch(function() {
+        // Offline fallback
         return caches.match('./index.html');
       });
     })
   );
 });
+
+// Message handler
+self.addEventListener('message', function(e) {
+  if (e.data && e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+  if (e.data && e.data.type === 'GET_VERSION') {
+    e.ports[0].postMessage({ version: CACHE_NAME });
+  }
+});
+
+console.log('[SW] AL BOWRY v16 Service Worker loaded');
